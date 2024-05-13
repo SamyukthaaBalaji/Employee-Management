7dd7e0fd8bf9477764f6ad682d63b997e15f0c0b
SamyukthaaBalaji <115622106+SamyukthaaBalaji@users.noreply.github.com>
2024-05-13 08:20:20 -0700

    added

--- client/src/components/Editemp.js
+++ client/src/components/Editemp.js
@@ -18,7 +18,7 @@ const Editemp = ({ emp }) => {
         phone_number: ph,
       };
       const response = await fetch(
-        `http://13.232.33.62:9000/user/updateemp/${emp.employee_id}`,
+        `http://52.66.214.26:9000/user/updateemp/${emp.employee_id}`,
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
@@ -44,7 +44,7 @@ const Editemp = ({ emp }) => {
         phone_number: ph,
       };
       const update = await fetch(
-        `http://13.232.33.62:9000/user/updateemp/${emp.employee_id}`,
+        `http://52.66.214.26/:9000/user/updateemp/${emp.employee_id}`,
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
--- client/src/components/User.js
+++ client/src/components/User.js
@@ -17,7 +17,7 @@ const User = () => {
     const getuserdet = async (employee_id) => {
       try {
         const det = await fetch(
-          `https://13.232.33.62:9000/user/empdet/${employee_id}`
+          `http://52.66.214.26:9000/user/empdet/${employee_id}`
         );
         const data = await det.json();
         console.log(data);
@@ -37,7 +37,7 @@ const User = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://13.232.33.62:9000/user/empdet", {
+      const response = await fetch("http://52.66.214.26:9000/user/empdet", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
